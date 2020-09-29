e0456114a6b272737d3f623e586dae9f15fce92f
Rikkey <rikkeypaal@gmail.com>
2020-09-30 00:02:27 +0100

    Improved LengthOfLongestSubstring Replaced the queue representing the search window with two integers pointing to windows boundaries. This greatly reduced memory overhead, and slightly reduced overall runtime.

--- src/main/scala/leetcode/medium/LengthOfLongestSubstring.scala
+++ src/main/scala/leetcode/medium/LengthOfLongestSubstring.scala
@@ -6,33 +6,44 @@ import scala.collection.mutable
 
 object LengthOfLongestSubstring {
 
+  /**
+   * Finds the length of the longest substring without any repeating characters
+   * Beats Approx: 88.13% runtime, 55.00% memory
+   *
+   * @param s target string to search though
+   * @return the length of the longest substring that contains no duplicate characters
+   */
   def lengthOfLongestSubstring(s: String): Int = {
     // current longest substring found
     var longest = 0
     // current section of the string being looked at
-    val window = mutable.Queue[Char]()
+    var windowStart = 0
+    var windowEnd = 0
     // set of characters in the window
-    var windowSet = mutable.Set[Char]()
+    val windowSet = mutable.Set[Char]()
 
     for (c <- s) {
       if (windowSet.contains(c)) {
         // update longest
-        longest = math.max(longest, window.size)
+        longest = math.max(longest, windowEnd - windowStart)
 
         // update window
         // don't need to remove c from set just from window so dequeue after
-        var last = window.dequeue()
+        var last = s(windowStart)
+        windowStart += 1
         while (last != c) {
           windowSet -= last
-          last = window.dequeue()
+          last = s(windowStart)
+          windowStart += 1
         }
       } else {
         windowSet += c
       }
-      window.enqueue(c)
+      windowEnd += 1
     }
-    math.max(longest, window.size)
+    math.max(longest, windowEnd - windowStart)
   }
+
 
   def main(args: Array[String]): Unit = {
     test(lengthOfLongestSubstring, "abcabcbb", 3)
