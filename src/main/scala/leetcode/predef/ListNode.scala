a8a064b0294cf2c7118eb87c1d7f6d22f655ebc8
Rikkey <rikkeypaal@gmail.com>
2021-07-02 11:26:32 +0100

    Added toString method to ListNode for easier comparison

--- src/main/scala/leetcode/predef/ListNode.scala
+++ src/main/scala/leetcode/predef/ListNode.scala
@@ -3,4 +3,11 @@ package leetcode.predef
 class ListNode(_x: Int = 0, _next: ListNode = null) {
 	var next: ListNode = _next
 	var x: Int = _x
+
+	override def toString: String =
+		if (next != null) {
+			s"$x," + next.toString
+		} else {
+			x + ""
+		}
 }
