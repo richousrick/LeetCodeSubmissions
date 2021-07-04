7e96fbe8fb45676684b6e16490fc90e635b6177c
Rikkey <rikkeypaal@gmail.com>
2021-07-04 12:01:57 +0100

    Added method description for RemoveDuplicates

--- src/main/scala/leetcode/easy/RemoveDuplicates.scala
+++ src/main/scala/leetcode/easy/RemoveDuplicates.scala
@@ -2,6 +2,12 @@ package leetcode.easy
 
 object RemoveDuplicates {
 
+	/**
+	 * Removes all duplicate integers from a sorted array in place.
+	 *
+	 * @param nums array of integers to remove the duplicates from
+	 * @return the size of the updated array.
+	 */
 	def removeDuplicates(nums: Array[Int]): Int =
 		if (nums.isEmpty) {
 			// As the lowest value the return case can return is 1, this case is necessary
